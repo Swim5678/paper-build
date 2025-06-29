c1e2c8e81d5329b01b4284a8c3f2a8ebc0bbf60b
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-06-29 22:09:27 +0800

    deleted:    .github/workflows/blank.yml 	new file:   .github/workflows/paper.yml 	new file:   build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone -b dev/1.21.7 https://github.com/PaperMC/Paper
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+cd Paper
+./gradlew applyPatches
+./gradlew createMojmapBundlerJar
