e0918be8a02513bf2c77499f7746c690e659b21d
swim5678 <154345558+Swim5678@users.noreply.github.com>
2025-09-17 10:45:11 +0800

    Clone specific branch of PaperMC repository

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/PaperMC/Paper
+git clone -b dev/1.21.9 https://github.com/PaperMC/Paper
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Paper
